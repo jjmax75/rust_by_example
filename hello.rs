296282045bd449bf0440195b1ed55d9c2acb1556
John Behan <behanj@gmail.com>
2020-03-12 18:34:17 +0000

    implememnt display for struct

--- hello.rs
+++ hello.rs
@@ -13,12 +13,44 @@ struct Person<'a> {
   age: u8
 }
 
-
 struct DisplayableStruct(i32);
 
 impl fmt::Display for DisplayableStruct {
   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
     write!(f, "{}", self.0)
+  }
+}
+
+#[derive(Debug)]
+struct MinMax(i64, i64);
+
+impl fmt::Display for MinMax {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    write!(f, "{}, {}", self.0, self.1)
+  }
+}
+
+#[derive(Debug)]
+struct Point2D {
+  x: f64,
+  y: f64
+}
+
+impl fmt::Display for Point2D {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    write!(f, "x: {}, y: {}", self.x, self.y)
+  }
+}
+
+#[derive(Debug)]
+struct Complex {
+  real: f32,
+  imag: f32
+}
+
+impl fmt::Display for Complex {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    write!(f, "{} + {}i", self.real, self.imag)
   }
 }
 
@@ -66,4 +98,29 @@ fn main() {
   println!("{:#?}", john);
 
   println!("Display of `DisplayableStruct(5)` - {}", DisplayableStruct(5));
+
+  let minmax = MinMax(0, 14);
+
+  println!("Compare structures:");
+  println!("Display: {}", minmax);
+  println!("Debug: {:?}", minmax);
+
+  let big_range = MinMax(-100, 300);
+  let small_range = MinMax(-3, 3);
+
+  println!("The big range is {big} and the small range is {small}",
+    small = small_range,
+    big = big_range);
+
+  let point = Point2D { x: 3.3, y: 7.2 };
+
+  println!("Compare points:");
+  println!("Display: {}", point);
+  println!("Debug: {:?}", point);
+
+  let complex = Complex { real: 3.3, imag: 7.2 };
+
+  println!("Compare complex:");
+  println!("Display: {}", complex);
+  println!("Debug: {:?}", complex);
 }

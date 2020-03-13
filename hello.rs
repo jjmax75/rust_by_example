782b236fcbc3d314a51fe0bfeb1be895f6d13c07
John Behan <behanj@gmail.com>
2020-03-13 16:35:15 +0000

    display for a complex type

--- hello.rs
+++ hello.rs
@@ -54,6 +54,23 @@ impl fmt::Display for Complex {
   }
 }
 
+struct List(Vec<i32>);
+
+impl fmt::Display for List {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    let vec = &self.0;
+
+    write!(f, "[")?;
+
+    for (index, v) in vec.iter().enumerate() {
+      if index != 0 { write!(f, ", ",)?; }
+      write!(f, "{}: {}", index, v)?;
+    }
+
+    write!(f, "]")
+  }
+}
+
 fn main() {
   println!("Hello world!");
   println!("I'm a Rustacean");
@@ -123,4 +140,8 @@ fn main() {
   println!("Compare complex:");
   println!("Display: {}", complex);
   println!("Debug: {:?}", complex);
+
+  let v = List(vec![1, 2, 3]);
+
+  println!("{}", v);
 }

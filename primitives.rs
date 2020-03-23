2657ffc12b993a43d1c50a05e384228ba01d9260
John Behan <behanj@gmail.com>
2020-03-23 17:03:37 +0000

    arrays and slices

--- primitives.rs
+++ primitives.rs
@@ -1,3 +1,10 @@
+use std::mem;
+
+fn analyse_slice(slice: &[i32]) {
+  println!("first element of the slice is {}", slice[0]);
+  println!("the slice has {} elements", slice.len());
+}
+
 #[derive(Debug)]
 struct Matrix(f32, f32, f32, f32);
 
@@ -79,4 +86,24 @@ fn main() {
 
   let matrix = Matrix(1.1, 1.2, 1.3, 1.4);
   println!("{:?}", matrix);
+
+  // arrays and slices
+  let xs: [i32; 5] = [1, 2, 3, 4, 5];
+  let ys: [i32; 500] = [0; 500];
+
+  println!("first element of the array is {}", xs[0]);
+  println!("second element of the array is {}", xs[1]);
+
+  println!("array size is {}", xs.len());
+
+  println!("array occupies {} bytes", mem::size_of_val(&xs));
+
+  println!("borrow the whole array as a slice");
+  analyse_slice(&xs);
+
+  println!("borrow a section of the array as a slice");
+  analyse_slice(&ys[1 .. 4]);
+
+  // compile error as this index is out of bounds
+  // println!("{}", xs[5]);
 }
